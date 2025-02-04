60f6edca109b52f865692ea96cf3efbf4abca9dc
Joey Blackwell <blackwellj6241@gmail.com>
2025-02-04 15:51:46 -0600

    added transient state

--- scripts/location.js
+++ scripts/location.js
@@ -1,6 +1,9 @@
+import { setSocioLocationId } from "./transientState.js"
+
 export const locationTypeChoices = async () => {
     const response = await fetch ("http://localhost:8088/socioLocations")
     const locations = await response.json() 
+    document.addEventListener("change", handleLocationChange)
 
     let choicesHTML = "<h2>Which type of area do you live in?</h2>"
     for (const location of locations) {
@@ -8,4 +11,11 @@ export const locationTypeChoices = async () => {
         
     }
     return choicesHTML
+}
+
+const handleLocationChange = (changeEvent) => {
+    if(changeEvent.target.name === "location") {
+        const convertedToInt = parseInt(changeEvent.target.value)
+        setSocioLocationId(convertedToInt)
+    }
 }
